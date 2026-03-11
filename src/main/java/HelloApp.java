ceda1725554dad07ec91aad4eb1b870d2bf48a27
Satviik Daga <sd3458@srmist.edu.in>
2026-03-11 15:51:15 +0530

    Coded UC1: Display Hello World in console

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,3 +1,10 @@
+ /*
+ HelloApp.java – A simple Java application that Displays "Hello, World!" to the console.
+ @author Satviik Daga
+ @version 1.0
+ */
 public class HelloApp {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
