5d0a9af649c590aa12b2563416898eede443e6fc
sd3458-glitch <sd3458@srmist.edu.in>
2026-03-18 16:08:00 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,15 +1,14 @@
  /*
  HelloApp.java – A simple Java application that Displays "Hello, World!" to the console.
  @author Satviik Daga
- @version 2.0
+ @version 3.0
  */
  public class HelloApp {
     public static void main(String[] args) {
+        String name="World";
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
+            name = args[0];
         }
+        System.out.println("Hello, " + name + "!");
     }
 }
