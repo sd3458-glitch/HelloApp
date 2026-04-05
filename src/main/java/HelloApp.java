58c31af6f2319f74ce5ccfdbd0861cbcb2d01f07
sd3458-glitch <sd3458@srmist.edu.in>
2026-04-05 23:59:01 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,14 +1,22 @@
  /*
  HelloApp.java – A simple Java application that Displays "Hello, World!" to the console.
  @author Satviik Daga
- @version 3.0
+ @version 4.0
  */
  public class HelloApp {
     public static void main(String[] args) {
-        String name="World";
+        String result="";
         if (args.length > 0) {
-            name = args[0];
+            for(int i=0;i<args.length;i++){
+                result+=args[i];
+            
+            if(i!=args.length-1){
+                result+=", ";
+                }
+            }
+        } else{
+            result+="World";
         }
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + result + "!");
     }
 }
