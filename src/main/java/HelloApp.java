35ef334966ff9dd71b678135c8fd647e62aa1600
sd3458-glitch <sd3458@srmist.edu.in>
2026-04-06 00:13:48 +0530

    Sync before merge

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,22 +1,18 @@
  /*
  HelloApp.java – A simple Java application that Displays "Hello, World!" to the console.
  @author Satviik Daga
- @version 4.0
+ @version 5.0
  */
  public class HelloApp {
     public static void main(String[] args) {
-        String result="";
-        if (args.length > 0) {
-            for(int i=0;i<args.length;i++){
-                result+=args[i];
-            
-            if(i!=args.length-1){
-                result+=", ";
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                if (name != null && !name.trim().isEmpty()) {
+                    System.out.println("Hello, " + name + "!");
                 }
             }
-        } else{
-            result+="World";
         }
-        System.out.println("Hello, " + result + "!");
     }
 }
